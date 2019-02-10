7b2fc8f956e4ba9a6fee0c11a42a5bf561c13860
jadlao <jadlao@gmail.com>
2019-02-10 18:57:33 +1100

    added camera

--- public/assets/scenes/Level2.js
+++ public/assets/scenes/Level2.js
@@ -5,7 +5,7 @@ export default class Level2 extends Phaser.Scene {
 		});
 	}
 	preload() {
-    this.load.image('star', '/assets/sprites/star.png')
+    this.load.image('got', '/assets/sprites/got.jpg')
 		this.load.spritesheet('dude', '/assets/sprites/dude.png', {
 			frameWidth: 32,
 			frameHeight: 48
@@ -13,8 +13,49 @@ export default class Level2 extends Phaser.Scene {
 		}
 
 	create() {
-    this.dude = this.add.sprite(100, 200, 'dude').setInteractive({draggable: true}).setDepth(2)
-    this.star = this.add.sprite(500, 200, 'star').setScale(3, 3).setInteractive({dropZone: true}).setDepth(1)
+    // camera
+    this.map = this.add.sprite(0, 0, 'got').setDepth(2).setOrigin(0,0).setDepth(0)
+    this.physics.world.setBounds(0, 0, this.map.width, this.map.height)
+    this.mainCamera = this.cameras.main
+    this.mainCamera.setViewport(0,0, this.game.config.width, this.game.config.height)
+    this.mainCamera.setZoom(1)
+    this.mainCamera.setBounds(0,0,this.map.width, this.map.height) // camera stops at world bound
+    //this.mainCamera.setScroll(200, 500)
+    //this.mainCamera.centerToBounds()
+      //.setInteractive({draggable: true}).setDepth(2)
+    // this.physics.add.existing(this.dude, 0)
+    // this.dude.body.setMass(25)
+
+    // use this way
+    this.player1 = this.physics.add.sprite(100, 300, 'dude').setScale(3,3).setOrigin(0,0).setOffset(0, 8)
+    this.player1.setSize(this.player1.body.width, this.player1.body.height - 8, false) // very important
+    console.log(this.player1)
+    this.player2 = this.physics.add.sprite(200, 150, 'dude') 
+    this.player2.setMass(25)
+
+    // world bound
+    this.physics.world.setBoundsCollision()
+    this.player1.setCollideWorldBounds(true)
+    this.player2.setCollideWorldBounds(true)
+    //this.physics.world.setBounds(200, 0, 400, 300)
+
+    // colliders - pass in game objects and callback
+    // this.physics.add.collider(this.player1, this.player2, () => {
+    //   console.log('collided')
+    // })
+    this.player1.setImmovable()
+    console.log(this.player1.body.blocked) // where body is being blocked
+    console.log(this.player1.body.touching)
+    this.location = 0
+
+    // overlap - e.g. mario collectin coins
+    this.physics.add.overlap(this.player1, this.player2, () => {
+      console.log('overlapped')
+    })
+
+      
+    this.star = this.add.sprite(500, 200, 'star').setScale(3, 3).setDepth(1)
+      //.setInteractive({dropZone: true}).setDepth(1)
 
     // KEYBOARD
     // this.input.enabled = true;
@@ -29,16 +70,18 @@ export default class Level2 extends Phaser.Scene {
     // console.log(this.input) // see methods in input object
 
     // MOUSE
-    this.input.on('dragenter', (pointer, gameObject, dropZone) => {
-      this.star.setTint(0x00ff00)
-    })
-    this.input.on('dragleave', (pointer, gameObject, dropZone) => {
-      this.star.clearTint()
-    })
-    this.input.on('drop', (pointer, gameObject, dropZone) => {
-
-    })
-    this.input.mouse.disableContextMenu() // disables right click menu pop up
+    // this.input.on('dragenter', (pointer, gameObject, dropZone) => {
+    //   this.star.setTint(0x00ff00)
+    // })
+    // this.input.on('dragleave', (pointer, gameObject, dropZone) => {
+    //   this.star.clearTint()
+    // })
+    // this.input.on('drop', (pointer, gameObject, dropZone) => {
+    //   gameObject.x = dropZone.x
+    //   gameObject.y = dropZone.y
+    //   dropZone.clearTint()
+    // })
+    // this.input.mouse.disableContextMenu() // disables right click menu pop up
 
     // this.input.on('pointerdown', pointer => {
     //   if (pointer.rightButtonDown()) {
@@ -66,20 +109,20 @@ export default class Level2 extends Phaser.Scene {
     //   this.dude.setScale(this.dude.scaleX + 1, this.dude.scaleY + 1)
     // })
 
-    this.dude.on('dragstart', (pointer, dragX, dragY) => {
-      console.log('Drag start')
-    })
+    // this.dude.on('dragstart', (pointer, dragX, dragY) => {
+    //   console.log('Drag start')
+    // })
 
-    this.dude.on('drag', (pointer, dragX, dragY) => {
-      console.log(`I'm dragging the dude`)
-      this.dude.setX(dragX)
-      this.dude.setY(dragY)
-    })
+    // this.dude.on('drag', (pointer, dragX, dragY) => {
+    //   console.log(`I'm dragging the dude`)
+    //   this.dude.setX(dragX)
+    //   this.dude.setY(dragY)
+    // })
 
-    this.dude.on('dragend', (pointer, dragX, dragY) => {
-      console.log('Drag end')
-      this.dude.setScale(this.dude.scaleX + 1, this.dude.scaleY + 1)
-    })
+    // this.dude.on('dragend', (pointer, dragX, dragY) => {
+    //   console.log('Drag end')
+    //   this.dude.setScale(this.dude.scaleX + 1, this.dude.scaleY + 1)
+    // })
     
 }
 
@@ -101,5 +144,17 @@ export default class Level2 extends Phaser.Scene {
     //   this.dude.setY(this.dude.y - 50)
     //   //console.log(this.dude.y)
     // }
+
+    // player collision
+    // if (this.player1.body.blocked.down) {
+    //   console.log('DIED')
+    // }
+    // if (this.player1.body.touching.up) {
+    //   console.log('touched above')
+    // }
+
+    // camera
+    this.location += 10
+    this.mainCamera.scrollY = this.location
   }
 }
